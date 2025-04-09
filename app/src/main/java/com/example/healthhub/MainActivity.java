349b9b7d80707ca204091b287acc8f2730b4aa82
Phlorion <gigadroll44thebest@gmail.com>
2025-04-09 23:36:59 +0300

    first admin screen

--- app/src/main/java/com/example/healthhub/MainActivity.java
+++ app/src/main/java/com/example/healthhub/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.healthhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent intent = new Intent(this, EnterAdminUserActivity.class);
+        startActivity(intent);
+
     }
 }
