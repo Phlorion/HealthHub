b0f1c85324af3514d7b8159b58f00f83c3bc28b1
victorkekas <vikor7007@gmail.com>
2025-05-07 14:36:34 +0300

    toast aded to imag buttons

--- app/src/main/java/com/example/healthhub/AdminMain.java
+++ app/src/main/java/com/example/healthhub/AdminMain.java
@@ -1,6 +1,9 @@
 package com.example.healthhub;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,53 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AdminMain extends AppCompatActivity {
 
+
+    ImageButton setPersonalInfo;
+    ImageButton setMedication;
+    ImageButton setHome;
+    ImageButton setFavContact;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+
+        setPersonalInfo = (ImageButton) findViewById(R.id.set_Personal_Info_btn);
+        setMedication = (ImageButton) findViewById(R.id.set_Medication_btn);
+        setHome = (ImageButton) findViewById(R.id.set_Home_btn);
+        setFavContact = (ImageButton) findViewById(R.id.set_Fav_Contacts_btn);
+
+        setPersonalInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminMain.this,"setPersonal", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        setHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminMain.this,"setHome", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        setMedication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminMain.this,"setMedication", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        setFavContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminMain.this,"setFavoriteCont", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
